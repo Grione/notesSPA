8e5c7e8d57bc3903bb5c67d834ad8dbbebbcf23a
Grione <greeb.spb@gmail.com>
2021-09-11 17:58:34 +0300

    add validate form

--- src/components/add-note/add-note.js
+++ src/components/add-note/add-note.js
@@ -4,9 +4,15 @@ const AddNote = ({ onAdd, onHide }) => {
 	const [ title, setTitle ] = useState('');
 	const [ text, setText ] = useState('');
 	const [ tags, setTags ] = useState('');
+	const [ validate, setValidate ] = useState(false);
 
 	const onSubmit = (e) => {
 		e.preventDefault();
+
+		if (!title && !text) {
+			setValidate(true);
+			return;
+		}
 
 		onAdd({ title, text }, tags);
 
@@ -23,13 +29,28 @@ const AddNote = ({ onAdd, onHide }) => {
 				<div className="field">
 					<label className="label">Title</label>
 					<div className="control">
-						<input type="text" className="input" value={title} onChange={(e) => setTitle(e.target.value)} />
+						<input
+							type="text"
+							className="input"
+							value={title}
+							onChange={(e) => {
+								setTitle(e.target.value);
+								setValidate(false);
+							}}
+						/>
 					</div>
 				</div>
 				<div className="field">
 					<label className="label">Text</label>
 					<div className="control">
-						<textarea className="textarea" value={text} onChange={(e) => setText(e.target.value)} />
+						<textarea
+							className="textarea"
+							value={text}
+							onChange={(e) => {
+								setText(e.target.value);
+								setValidate(false);
+							}}
+						/>
 					</div>
 				</div>
 				<div className="field">
@@ -44,6 +65,13 @@ const AddNote = ({ onAdd, onHide }) => {
 					</div>
 				</div>
 				<button className="button is-link">Add</button>
+				{validate ? (
+					<div className="notification is-warning mt-1">
+						<strong>Please fill in at least one text field</strong>
+					</div>
+				) : (
+					''
+				)}
 			</form>
 		</div>
 	);
