c18efea649a3f1e7568d16714abf0cc123cc0753
Grione <greeb.spb@gmail.com>
2021-11-29 13:16:26 +0300

    add react-router-dom

--- src/App.js
+++ src/App.js
@@ -1,22 +1,27 @@
 import { useEffect } from 'react';
 import Dashboard from './components/dashboard/dashboard';
+import Login from './components/login/login';
 import { useSelector, useDispatch } from 'react-redux';
 import { fetchNotes } from './state/action-creators';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
 
 const App = () => {
 	const state = useSelector((state) => state.notes);
 
-  const dispatch = useDispatch();
+	const dispatch = useDispatch();
 
 	useEffect(() => {
 		dispatch(fetchNotes());
 	}, []);
 
 	return (
-		<div className="wrapper has-background-dark">
-			<div className="container is-fluid">
-				<Dashboard state={state} />
-			</div>
+		<div>
+			<BrowserRouter>
+				<Routes>
+					<Route path="/dashboard" element={<Dashboard state={state} />} />
+					<Route path="/" element={<Login />} />
+				</Routes>
+			</BrowserRouter>
 		</div>
 	);
 };
