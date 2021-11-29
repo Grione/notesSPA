4431d100b054c3c1ddc0c2923e12ce8ecae3b4b8
Grione <greeb.spb@gmail.com>
2021-11-29 12:40:27 +0300

    add dashboard layout

--- src/components/dashboard/dashboard.js
+++ src/components/dashboard/dashboard.js
@@ -4,19 +4,19 @@ import TagsList from '../tags-ist/tags-list';
 import NotesList from '../notes-list/notes-list';
 import AddNote from '../add-note/add-note';
 
-const Dashboard = ({state}) => {
-  const [ showAddNote, setShowAddNote ] = useState(false);
-  const [ tags, setTags ] = useState([]);
+const Dashboard = ({ state }) => {
+	const [ showAddNote, setShowAddNote ] = useState(false);
+	const [ tags, setTags ] = useState([]);
 
-  const sortNotes = (checked, value) => {
+	const sortNotes = (checked, value) => {
 		setTags(!tags.includes(value) && checked ? [ ...tags, value ] : tags.filter((n) => n !== value));
 	};
 
-  const filteredNotes = state.filter((note) => {
+	const filteredNotes = state.filter((note) => {
 		return !tags.length || tags.find((item) => note.tags.includes(item));
 	});
 
-  	// Get all tags
+	// Get all tags
 	const getAllTags = (notes) => {
 		const newTags = new Set();
 		if (notes.length) {
@@ -29,14 +29,18 @@ const Dashboard = ({state}) => {
 	};
 
 	return (
-		<div className="columns has-text-light">
-			<div className="wrapper-column column has-background-dark">
-				<AddNoteButton onAdd={() => setShowAddNote(!showAddNote)} isDisabled={showAddNote} />
-				<TagsList tags={getAllTags(state)} onSort={sortNotes} />
-			</div>
-			<div className="wrapper-column column has-background-dark is-three-quarters">
-				{showAddNote ? <AddNote onHide={() => setShowAddNote(false)} /> : ''}
-				<NotesList notes={filteredNotes} />
+		<div className="wrapper has-background-dark">
+			<div className="container is-fluid">
+				<div className="columns has-text-light">
+					<div className="wrapper-column column has-background-dark">
+						<AddNoteButton onAdd={() => setShowAddNote(!showAddNote)} isDisabled={showAddNote} />
+						<TagsList tags={getAllTags(state)} onSort={sortNotes} />
+					</div>
+					<div className="wrapper-column column has-background-dark is-three-quarters">
+						{showAddNote ? <AddNote onHide={() => setShowAddNote(false)} /> : ''}
+						<NotesList notes={filteredNotes} />
+					</div>
+				</div>
 			</div>
 		</div>
 	);
