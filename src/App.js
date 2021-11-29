4d9163e5d2445c13fd23ab9e79d3060fa4a8b994
Grione <greeb.spb@gmail.com>
2021-11-29 14:41:51 +0300

    create auth server

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
 import Dashboard from './components/dashboard/dashboard';
 import Login from './components/login/login';
 import { useSelector, useDispatch } from 'react-redux';
@@ -7,19 +7,22 @@ import { BrowserRouter, Route, Routes } from 'react-router-dom';
 
 const App = () => {
 	const state = useSelector((state) => state.notes);
-
 	const dispatch = useDispatch();
-
 	useEffect(() => {
 		dispatch(fetchNotes());
 	}, []);
+
+	const [ token, setToken ] = useState();
+
+	if (!token) {
+		return <Login setToken={setToken} />;
+	}
 
 	return (
 		<div>
 			<BrowserRouter>
 				<Routes>
 					<Route path="/dashboard" element={<Dashboard state={state} />} />
-					<Route path="/" element={<Login />} />
 				</Routes>
 			</BrowserRouter>
 		</div>
